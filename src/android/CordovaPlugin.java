93b47bf5fed4d2979b756c613c57a6ac089cabe0
Maksim Chemerisuk <chemerisuk@gmail.com>
2018-05-13 13:51:06 +0300

    implement support for arguments

--- src/android/CordovaPlugin.java
+++ src/android/CordovaPlugin.java
@@ -3,6 +3,7 @@ package by.chemerisuk.cordova.support;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.JSONException;
 
 import java.lang.reflect.Method;
@@ -14,7 +15,7 @@ public class CordovaPlugin extends org.apache.cordova.CordovaPlugin {
     private Map<String, Method> methodsMap;
 
     @Override
-    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (methodsMap == null) {
             methodsMap = new HashMap<String, Method>();
             for (Method method : this.getClass().getDeclaredMethods()) {
@@ -27,8 +28,8 @@ public class CordovaPlugin extends org.apache.cordova.CordovaPlugin {
                     methodsMap.put(methodAction, method);
 
                     try {
-                        // improve performance for future invokations:
                         // suppress Java language access checks
+                        // to improve performance of future calls
                         method.setAccessible(true);
                     } catch (SecurityException e) {
                         e.printStackTrace();
@@ -40,7 +41,7 @@ public class CordovaPlugin extends org.apache.cordova.CordovaPlugin {
         Method method = methodsMap.get(action);
         if (method != null) {
             try {
-                method.invoke(this, args, callbackContext);
+                method.invoke(this, collectArgs(method, args, callbackContext));
                 return true;
             } catch (ReflectiveOperationException e) {
                 e.printStackTrace();
@@ -48,5 +49,25 @@ public class CordovaPlugin extends org.apache.cordova.CordovaPlugin {
         }
 
         return false;
+    }
+
+    protected Object[] collectArgs(Method method, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Class<?>[] argTypes = method.getParameterTypes();
+        Object[] methodArgs = new Object[argTypes.length];
+
+        for (int i = 0; i < argTypes.length; ++i) {
+            Class<?> argType = argTypes[i];
+            if (CallbackContext.class.equals(argType)) {
+                methodArgs[i] = callbackContext;
+            } else if (JSONArray.class.equals(argType)) {
+                methodArgs[i] = args.optJSONArray(i);
+            } else if (JSONObject.class.equals(argType)) {
+                methodArgs[i] = args.optJSONObject(i);
+            } else {
+                methodArgs[i] = args.get(i);
+            }
+        }
+
+        return methodArgs;
     }
 }
