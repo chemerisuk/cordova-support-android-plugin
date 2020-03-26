bab15faaf7a7206d493c594add9adf5fe84925dc
Maksim Chemerisuk <chemerisuk@gmail.com>
2020-03-26 11:00:58 +0300

    interface methods are always public in Java

--- src/android/CordovaMethod.java
+++ src/android/CordovaMethod.java
@@ -1,15 +1,15 @@
 package by.chemerisuk.cordova.support;
 
-import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin.ExecutionThread;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin.ExecutionThread;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface CordovaMethod {
-    public ExecutionThread value() default ExecutionThread.MAIN;
-    public String action() default "";
+    ExecutionThread value() default ExecutionThread.MAIN;
+    String action() default "";
 }
