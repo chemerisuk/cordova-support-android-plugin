bafe54682449d21e923b85aaeb1b5d06503466b1
Maksim Chemerisiuk <chemerisuk@gmail.com>
2022-07-28 11:43:57 +0300

    Add androidx.annotation.Keep to CordovaMethod

--- src/android/CordovaMethod.java
+++ src/android/CordovaMethod.java
@@ -1,14 +1,15 @@
 package by.chemerisuk.cordova.support;
 
+import androidx.annotation.Keep;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin.ExecutionThread;
-
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+@Keep
 public @interface CordovaMethod {
     ExecutionThread value() default ExecutionThread.MAIN;
     String action() default "";
